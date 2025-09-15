aedac515c469871014362c9cd8853865adde80bc
Lanest <lanest.lin@oocl.com>
2025-09-15 15:16:57 +0800

    feat: add DefaultLayout.css

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,18 +1,21 @@
 import {NavLink, Outlet} from "react-router";
+import "../DefaultLayout.css";
 
 export function DefaultLayout() {
-	return <div>
-		<header>
-			<nav>
-				<ul>
-					<li><NavLink to={"/"}>Home</NavLink></li>
-					<li><NavLink to={"/done"}>Done</NavLink></li>
-					<li><NavLink to={"/about"}>About us</NavLink></li>
-				</ul>
-			</nav>
-		</header>
-		<main>
-			<Outlet/>
-		</main>
-	</div>;
+	return (
+		<div className="default-layout">
+			<header>
+				<nav>
+					<ul>
+						<li><NavLink to={"/"}>Home</NavLink></li>
+						<li><NavLink to={"/done"}>Done</NavLink></li>
+						<li><NavLink to={"/about"}>About us</NavLink></li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<Outlet/>
+			</main>
+		</div>
+	);
 }
