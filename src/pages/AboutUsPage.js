8ff91a9fed6bbdbbd70a8c08ad03c58e7cb0ea2e
Lanest <lanest.lin@oocl.com>
2025-09-15 20:03:27 +0800

    feat: add antd in AboutUsPage.js

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,22 +1,43 @@
+import { Card, Typography, List } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
+
+const { Title, Paragraph } = Typography;
+
 export function AboutUsPage() {
+	const features = [
+		"Create new todo items",
+		"Mark item as completed",
+		"View list of completed items",
+		"Delete unwanted items",
+		"Edit item details",
+		"View item details"
+	];
+	
 	return (
-		<div className="about-us-page">
-			<h1>About Us</h1>
-			<p>Welcome to our Todo List application!</p>
-			
-			<h2>Application Features</h2>
-			<ul>
-				<li>Create new todo items</li>
-				<li>Mark tasks as completed</li>
-				<li>View list of completed tasks</li>
-				<li>Delete unwanted tasks</li>
-				<li>View task details</li>
-			</ul>
-			
-			<h2>How to Use</h2>
-			<p>On the home page, you can add new todo items, click on the task text to mark it as done, and click the "×" button to delete a task.</p>
-			<p>Use the navigation bar to access the completed tasks list and other pages.</p>
-			
+		<div>
+			<Card>
+				<Title level={1}>
+					<InfoCircleOutlined /> About Us
+				</Title>
+				<Paragraph>Welcome to our Todo List application!</Paragraph>
+				<Title level={2}>Application Features</Title>
+				<List
+					dataSource={features}
+					renderItem={item => (
+						<List.Item>
+							<Typography.Text strong>• {item}</Typography.Text>
+						</List.Item>
+					)}
+				/>
+				<Title level={2}>How to Use</Title>
+				<Paragraph>
+					On the home page, you can add new todo items, click on the task text to mark it as done,
+					and click the "×" button to delete a task.
+				</Paragraph>
+				<Paragraph>
+					Use the navigation bar to access the completed tasks list and other pages.
+				</Paragraph>
+			</Card>
 		</div>
 	);
 }
