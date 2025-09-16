457bbff9094ba33e51ac74d084532f3b693f93b7
Lanest <lanest.lin@oocl.com>
2025-09-16 16:53:36 +0800

    feat: interceptors response

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import {initState, TodoContext} from "./contexts/TodoContext";
 import {RouterProvider} from "react-router";
 import {routes} from "./routes/Routes";
 import {useToService} from "./useTodoService";
+import { unstableSetRender } from 'antd';
+import { createRoot } from 'react-dom/client';
+
+unstableSetRender((node, container) => {
+	container._reactRoot ||= createRoot(container);
+	const root = container._reactRoot;
+	root.render(node);
+	return async () => {
+		await new Promise((resolve) => setTimeout(resolve, 0));
+		root.unmount();
+	};
+});
 
 function App() {
 	const [state, dispatch] = useReducer(todoReducer, initState);
--- src/api/MockApi.js
+++ src/api/MockApi.js
@@ -1,7 +1,27 @@
 import axios from "axios";
+import {message} from "antd";
 
-export const api = axios.create({
-	baseURL: 'https://68c7ac9f5d8d9f51473288e2.mockapi.io',
-	headers: {'Content-Type': 'application/json'},
-	timeout: 10_000
+const api = axios.create({
+	baseURL: "http://localhost:8080/",
+	headers: {
+		"Content-Type": "application/json",
+	},
+	timeout: 10_000,
 })
+
+api.interceptors.response.use(
+	(response) =>
+	{
+		return response;
+	},
+	(error) =>
+	{
+		const { status, data } = error.response;
+		if (status === 400 || status === 404 || status === 500) {
+			message.error(data.message || "An error occurred. Please try again.");
+		}
+		return Promise.reject(error);
+	}
+);
+
+export {api};
